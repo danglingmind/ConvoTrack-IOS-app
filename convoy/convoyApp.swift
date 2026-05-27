01079f2854e71562e87cfced48d51e429129f8b0
Prateek Reddy <prateekreddy274@gmail.com>
2026-05-27 22:54:14 +0530

    Initial Commit

---
+++ convoy/convoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  convoyApp.swift
+//  convoy
+//
+//  Created by Prateek Reddy on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct convoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
